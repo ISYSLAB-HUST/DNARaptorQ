ec5fa122ba355a427282ac96e96fe21761fe1ed7
TwentyO <55370793+TwentyO@users.noreply.github.com>
2019-12-14 15:11:00 +0800

    Update seed_encode.py

--- seed_encode.py
+++ seed_encode.py
@@ -8,7 +8,7 @@ def int_to_sixteen(symbol_id):
     return symbol_id_16  # return symbol_ID in 2 bit Hexadecimal
 
 
-def sixteen_to_dna(symbol_ID_16):
+def sixteen_to_dna(symbol_id_16):
 
     codon = {'0': 'GCT', '1': 'TGT', '2': 'GAT', '3': 'GAA',
              '4': 'CAT', '5': 'ATG', '6': 'AAT', '7': 'CAA',
@@ -17,13 +17,13 @@ def sixteen_to_dna(symbol_ID_16):
     # This diction corresponds to genetic code preference in Arabidopsis thaliana
 
 
-    dna_symbol_ID = ''
+    dna_symbol_id = ''
 
-    for i in symbol_ID_16:
+    for i in symbol_id_16:
     
-        dna_symbol_ID += codon[i]
+        dna_symbol_id += codon[i]
 
-    return dna_symbol_ID  # return symbol ID coding in DNA
+    return dna_symbol_id  # return symbol ID coding in DNA
 
 
 
@@ -44,12 +44,12 @@ def int_to_dna(symbol_id):  # This function can directly translate symbol ID int
 
     #This step can be optimization. 4 corresponds to len of the DNA  code
 
-    dna_symbol_ID = ''
+    dna_symbol_id = ''
 
     for i in symbol_id_16:  # Translate symbol ID into DNA by dictionary
 
-        dna_symbol_ID += codon[i]
+        dna_symbol_id += codon[i]
 
-    return dna_symbol_ID               # return symbol ID coding in 6 np DNA
+    return dna_symbol_id               # return symbol ID coding in 6 np DNA
 
 
