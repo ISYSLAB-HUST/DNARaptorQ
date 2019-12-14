2f25d42c457d25176ba48b94c6025eb93a968eb0
TwentyO <55370793+TwentyO@users.noreply.github.com>
2019-12-14 15:00:51 +0800

    Create seed_encode.py
    There are two different way to translate symbol ID into DNA.  I found a built-in function can translate number into hexadecimal, 
    so we only need a function to translate hexadecimal number into DNA. 
    There are two questions. 1, we design this process on the base of register, some time register complicated problem.
    2, I don't find a easy to padding 0 to get a fixed length hexadecimal number , so use a ugly method.

---
+++ seed_encode.py
@@ -0,0 +1,55 @@
+def int_to_sixteen(symbol_id):
+
+    bin_data = '{0:08b}'.format(symbol_id)
+    # convert to a long string of binary values, only have low 8 bit of the symbol_ID
+
+    symbol_id_16 = '0' * (4-len(hex(int(bin_data, 2)))) + str(hex(int(bin_data, 2)))[2:]
+
+    return symbol_id_16  # return symbol_ID in 2 bit Hexadecimal
+
+
+def sixteen_to_dna(symbol_ID_16):
+
+    codon = {'0': 'GCT', '1': 'TGT', '2': 'GAT', '3': 'GAA',
+             '4': 'CAT', '5': 'ATG', '6': 'AAT', '7': 'CAA',
+             '8': 'TAT', '9': 'TGG', 'a': 'GTT', 'b': 'AGA',
+             'c': 'CCT', 'd': 'GGA', 'e': 'AAG', 'f': 'ACT'}
+    # This diction corresponds to genetic code preference in Arabidopsis thaliana
+
+
+    dna_symbol_ID = ''
+
+    for i in symbol_ID_16:
+    
+        dna_symbol_ID += codon[i]
+
+    return dna_symbol_ID  # return symbol ID coding in DNA
+
+
+
+
+
+
+
+
+def int_to_dna(symbol_id):  # This function can directly translate symbol ID into DNA
+
+    codon = {'0': 'GCT', '1': 'TGT', '2': 'GAT', '3': 'GAA',
+             '4': 'CAT', '5': 'ATG', '6': 'AAT', '7': 'CAA',
+             '8': 'TAT', '9': 'TGG', 'a': 'GTT', 'b': 'AGA',
+             'c': 'CCT', 'd': 'GGA', 'e': 'AAG', 'f': 'ACT'}
+    # This diction corresponds to genetic code preference in Arabidopsis thaliana
+
+    symbol_id_16 = '0' * (4-len(hex(symbol_id)))+str(hex(symbol_id))[2:]
+
+    #This step can be optimization. 4 corresponds to len of the DNA  code
+
+    dna_symbol_ID = ''
+
+    for i in symbol_id_16:  # Translate symbol ID into DNA by dictionary
+
+        dna_symbol_ID += codon[i]
+
+    return dna_symbol_ID               # return symbol ID coding in 6 np DNA
+
+
