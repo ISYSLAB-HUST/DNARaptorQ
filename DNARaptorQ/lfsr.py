2141d8c6cd89fbb4d6a8f3374d7b9839dbb39acb
wangleiofficial <7983060609@qq.com>
2019-12-20 19:50:13 +0800

    add lfsr.py;

---
+++ DNARaptorQ/lfsr.py
@@ -0,0 +1,32 @@
+"""
+LFSR module by Zhanghang
+"""
+
+
+def lfsr(state, mask):
+    # Galois lfsr:
+    result = state
+    nbits = mask.bit_length() - 1
+    while True:
+        result = (result << 1)
+        xor = result >> nbits
+        if xor != 0:
+            result ^= mask
+        yield result
+
+
+def lfsr16mask():
+    # this function returns a hard coded polynomial (0b10000000000101101).
+    # The polynomial corresponds to 1 + x^11 + x^13 + x^14 + x^16, which is known
+    # to repeat only after 16^2-1 tries. Don't change this number!
+    return 0b10000000000101101
+
+
+def pseudo_random(seed, len_payload):
+    # 32 Byte pseudo-random number generator
+    pr_block = int(len_payload / 2)
+    lf = lfsr(seed, lfsr16mask())
+    s = ''
+    for i in range(pr_block):
+        s += '{:016b}'.format(next(lf))
+    return int(s, 2)
